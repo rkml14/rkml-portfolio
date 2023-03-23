8bc64a27f4e55056db457c0ac1e500971bbe9d3e
rkml14 <119961747+rkml14@users.noreply.github.com>
2023-03-23 18:32:29 -0400

    fixed the images in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,5 @@
 import React from "react";
 
-//import icons 
-import github from "../images/github.png";
-import linkedin from "../images/linkedin.png";
-import instagram from "../images/instagram.png";
-import stackoverflow from "../images/stackoverflow.png";
-
 export default function Footer() {
    const year = new Date().getFullYear();
     return(
@@ -13,10 +7,10 @@ export default function Footer() {
         <div>
            
         {`Copyright @ ${year} Rebecca Lawrence `}
-        <a href="https://github.com/rkml14"><img src={github} alt="Github"/></a>
-        <a href="https://www.linkedin.com/in/rebecca-lawrence-762642265/"><img src={linkedin} alt="LinkedIn"/></a>
-        <a href="https://stackoverflow.com/users/21448112/rebecca-lawrence"><img src={stackoverflow} alt="Stack-Overflow"/></a>
-        <a href="https://www.instagram.com/bexknitting/"><img src={instagram} alt="Instagram"/></a>
+        <a href="https://github.com/rkml14"><img src={require('../assets/images/github.png')} alt="Github"/></a>
+        <a href="https://www.linkedin.com/in/rebecca-lawrence-762642265/"><img src={require('../assets/images/linkedin.png')} alt="LinkedIn"/></a>
+        <a href="https://stackoverflow.com/users/21448112/rebecca-lawrence"><img src={require('../assets/images/stackoverflow.png')} alt="Stack-Overflow"/></a>
+        <a href="https://www.instagram.com/bexknitting/"><img src={require('../assets/images/instagram.png')} alt="Instagram"/></a>
         </div>
         </footer>
     )
