f3cf63f7d37ae8c9312f0c222e5736041ea580f7
rkml14 <119961747+rkml14@users.noreply.github.com>
2023-03-23 17:48:28 -0400

    trying to set up footer

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import React from "react";
 import PortfolioContainer from "./components/PortfolioContainer";
+import 'bootstrap-css-only/css/bootstrap.min.css';
+
+
+
 
 const App = () => <PortfolioContainer />;
 
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,17 +1,27 @@
 import React from "react";
 
+//import icons 
+import github from "../images/github.png";
+import linkedin from "../images/linkedin.png";
+import instagram from "../images/instagram.png";
+import stackoverflow from "../images/stackoverflow.png";
+
 export default function Footer() {
    const year = new Date().getFullYear();
     return(
+        <footer className="footer">
         <div>
-
-            
-            {`Copyright @ Rebecca Lawrence ${year}`}
+           
+        {`Copyright @ ${year} Rebecca Lawrence `}
+        <a href="https://github.com/rkml14"><img src={github} alt="Github"/></a>
+        <a href="https://www.linkedin.com/in/rebecca-lawrence-762642265/"><img src={linkedin} alt="LinkedIn"/></a>
+        <a href="https://stackoverflow.com/users/21448112/rebecca-lawrence"><img src={stackoverflow} alt="Stack-Overflow"/></a>
+        <a href="https://www.instagram.com/bexknitting/"><img src={instagram} alt="Instagram"/></a>
         </div>
+        </footer>
     )
    
 };
-
 
 
 
