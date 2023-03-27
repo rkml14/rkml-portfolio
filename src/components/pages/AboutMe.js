f80ae0a947c75c2145f8b33e3d1476531a6b7e6d
rkml14 <119961747+rkml14@users.noreply.github.com>
2023-03-26 20:40:43 -0400

    fixed About me & portfolio cards

--- src/components/pages/AboutMe.js
+++ src/components/pages/AboutMe.js
@@ -4,19 +4,27 @@ import React from 'react';
 export default function AboutMe() {
   return (
     <div className="about-me">
-    <div>
-    <img src={require('../../images/headshot/rkml.png')} alt="Rebecca-with-sheep" />
+      <div className='my-name'>
+        <h1>About Me</h1>
+      </div>
+      <div className="image">
+        <img className="headshot" src={require('../../images/headshot/rkml.png')} alt="Rebecca-with-sheep" />
+      </div>
 
+      <p className="bio-line">Hi, I'm Rebecca, and I'm a web developer based in Toronto, Canada!
+      </p>
 
+      <p className="about-text">Currently I an enrolled in the University of Toronto's Full Stack Development Bootcamp, learning & developing my knowledge of JavaScript, HTML, CSS, MERN and more!  I decided to make a change to field I have a lot of passion for after creating several websites for other people!
+      </p>
+
+      <p className="about-text">I'm always seeking to further my knowledge, experience and to take on new challenges as I continue to grow as a developer!  As a former Credit Analyst, my logical and solutions-driven work ethic are skills I want to bring to my new field.
+      </p>
+
+      <p className="about-text">When I'm not coding, you can find me training my working Border Collies in sheepherding, competing in sheepdog trials and generally getting mucky on a farm! The ability to be thinking several steps ahead, and adapting on the fly help me bring a unique perspective to my work as a web developer.
+      </p>
 
     </div>
-    <div className="aboutme">
-      <h1>About Me</h1>
-      <p>Hi, I'm Rebecca!  I an an adaptable, passionate & solutions-driven web developer.   I'm always seeking to further my knowledge, experience and to take on new challenges as I continue to grow as a developer!  In my free time, I train working border collies to herd sheep and compete in sheepdog trials.   </p>
-    </div>
-</div>
-  )
-
+    )
 }
 
 
